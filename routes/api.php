31ad5736c7afdf738e93572856a97b2e59b87b25
victan78 <116087124+Victan78@users.noreply.github.com>
2023-05-05 21:13:28 +0200

    yguh

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
+Route::post('/logout', 'AuthController@logout')->middleware('auth:api');
+Route::get('/',function(){
+ return 'hello world';
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
